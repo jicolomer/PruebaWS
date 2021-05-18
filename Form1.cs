78a4af8e265951d9261055b96cab096e73c6ac5e
jicolomer <jicolomer@cadam.es>
2021-05-18 17:15:26 +0200

    20210518

--- Form1.cs
+++ Form1.cs
@@ -20,8 +20,7 @@ namespace PruebaWS
 
         private void Form1_Load(object sender, EventArgs e)
         {
-              
-            //Comment Final
+            //Comment Final 2
             Prueba.WS_IntegrAR p = new Prueba.WS_IntegrAR();
 
             try
